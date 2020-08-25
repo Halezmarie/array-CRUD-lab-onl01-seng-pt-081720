aba94b891decfafaac097950a87f37a58f92606f
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-25 03:50:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Pupper", "Doggo", "Pupperino", "Doggy"]
-  add_element_to_end_of_array.push ("Doggovinci")
+  add_element_to_end_of_array.push("Doggovinci")
 end
 
 def add_element_to_start_of_array(array, element)
